00e237a8dc837e3df5710a6a638e3b723efbf1cc
Bernardo Freitas Paulo da Costa <bernardo.paulo@fgv.br>
2025-10-14 13:48:09 -0300

    Add SAA value function to LDR comparison
    Since this is a linear-quadratic problem, all are equivalent (SAA up to
    Monte-Carlo integration)

--- examples/recursion/toy_vf.jl
+++ examples/recursion/toy_vf.jl
@@ -80,6 +80,40 @@ plt.plot(xs, ys, label="LDR Value Function")
 
 end
 
+# SAA value function
+begin
+Nscen = 100
+
+m = Model(Ipopt.Optimizer)
+set_silent(m)
+@variable(m, vi)
+@variable(m, 0 <= vf[1:Nscen] <= 1)
+@variable(m, gh[1:Nscen] >= 0.0)
+@variable(m, gt[1:Nscen] >= 0.0)
+@variable(m, inflow[1:Nscen])
+@constraint(m, [s=1:Nscen], vf[s] == vi - gh[s] + inflow[s])
+@constraint(m, [s=1:Nscen], gt[s] + gh[s] == demand)
+@objective(m, Min, (1/Nscen) * sum(gt[s]^2 + vf[s]^2/2 - vf[s] + 0.5 for s=1:Nscen))
+
+# Sample inflows
+inflow_dist = Uniform(0, 0.2)
+for s in 1:Nscen
+    fix(inflow[s], rand(inflow_dist))
+end
+
+# Solve the SAA
+optimize!(m)
+function eval_VF3(m, x)
+    fix(m[:vi], x)
+    JuMP.optimize!(m)
+    return JuMP.objective_value(m)
+end
+
+xs = 0:0.01:1
+ys = eval_VF3.(m, xs)
+plt.plot(xs, ys, label="SAA Value Function")
+end
+
 plt.legend()
 plt.title("Value Function Comparison")
 plt.savefig("1dtoy_value_function_comparison.pdf")
