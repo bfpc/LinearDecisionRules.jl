f3fe61d975b4cc3d376aeec15569a7afc2827aef
Bernardo Freitas Paulo da Costa <bernardofpc@im.ufrj.br>
2026-07-07 07:35:48 -0300

    Add simple 3 stage problem to test

--- test/test_stages.jl
+++ test/test_stages.jl
@@ -83,6 +83,81 @@ function test_first_stage_mixed_with_stage_warns()
     return nothing
 end
 
+function test_3_stage_newsvendor()
+    # Parameters
+    buy_cost = 10
+    return_value = 8
+    sell_value = 15
+
+    demand_max = 120
+    demand_min = 80
+    demand_distr = Distributions.Uniform(demand_min, demand_max)
+    cost_stage2_distr = Distributions.Uniform(8.0, 14.0)
+
+    # Model
+    ldr = LinearDecisionRules.LDRModel(HiGHS.Optimizer)
+    set_silent(ldr)
+
+    @variable(ldr, buy >= 0); set_attribute(buy, Stage(1))
+
+    @variable(ldr, sell2 >= 0); set_attribute(sell2, Stage(2))
+    @variable(ldr, ret2 >= 0); set_attribute(ret2, Stage(2))
+    @variable(ldr, demand2 in
+        LinearDecisionRules.Uncertainty(
+            distribution = demand_distr,
+            stage = 2
+            )
+    )
+    @variable(ldr, buy2 >= 0); set_attribute(buy2, Stage(2))
+    @variable(ldr, buy_cost2 in
+        LinearDecisionRules.Uncertainty(
+            distribution = cost_stage2_distr,
+            stage = 2
+            )
+    )
+
+    @variable(ldr, sell3 >= 0); set_attribute(sell3, Stage(3))
+    @variable(ldr, ret3 >= 0); set_attribute(ret3, Stage(3))
+    @variable(ldr, demand3 in
+        LinearDecisionRules.Uncertainty(
+            distribution = demand_distr,
+            stage = 3
+            )
+    )
+
+    @constraint(ldr, sell2 + ret2 <= buy + buy2)
+    @constraint(ldr, sell2 <= demand2)
+
+    @constraint(ldr, sell2 + ret2 + sell3 + ret3 <= buy + buy2)
+    @constraint(ldr, sell3 <= demand3)
+
+
+    @objective(ldr, Max,
+        - buy_cost * buy
+        - buy_cost2 * buy2
+        + return_value * ret2
+        + sell_value * sell2
+        + return_value * ret3
+        + sell_value * sell3
+    )
+
+    optimize!(ldr)
+
+    @test termination_status(ldr) in (MOI.OPTIMAL, MOI.LOCALLY_SOLVED)
+
+    @test objective_value(ldr) ≈ 940.0
+    @test objective_value(ldr; dual=true) ≈ 1000.0
+
+    # Simple no-dependence check on the decision rules
+    @test LinearDecisionRules.get_decision(ldr, ldr[:buy], ldr[:demand2]) == 0.0
+    @test LinearDecisionRules.get_decision(ldr, ldr[:buy], ldr[:demand3]) == 0.0
+    for var in (:sell2, :ret2, :buy2)
+        @test LinearDecisionRules.get_decision(ldr, ldr[var], ldr[:demand3]) == 0.0
+    end
+
+    return nothing
+end
+
 end # TestStages module
 
 TestStages.runtests()
